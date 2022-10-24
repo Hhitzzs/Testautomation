7202a9a81b53fd282b84622369c0cf8d91fc6bf4
Artem <95497573+Hhitzzs@users.noreply.github.com>
2022-10-24 11:19:13 +0300

    Add files via upload

---
+++ testing/src/test/java/testExampele.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class testExampele {
+   @Test
+    void showAssertion() {
+       Assertions.assertEquals(1,1);
+   }
+}
+
+
+
+
+
